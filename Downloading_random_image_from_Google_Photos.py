2a65645087d145232b3dcc84a8fd4e917e546406
Aryan Garg <aryan.raj.garg@gmail.com>
2021-02-23 00:54:29 +0530

    Added Hashtag support Cleaned up code and added comments

--- Downloading_random_image_from_Google_Photos.py
+++ Downloading_random_image_from_Google_Photos.py
@@ -1,6 +1,5 @@
 from Uploading_Photo_to_Instagram import *
 from pandas import DataFrame
-from time import sleep
 from random import choice
 import requests
 import os
@@ -8,8 +7,12 @@ import traceback
 from json import load
 import datetime
 from shutil import copy
+from datetime import date
+import calendar
 
-
+my_date = date.today()
+today = calendar.day_name[my_date.weekday()]
+# -------- ^ Initiations || v META CODE --------
 Base_URL_list = []
 x = datetime.datetime.now()
 cur = x.strftime('%Y-%m-%d')
@@ -19,17 +22,24 @@ with open('./userinfo.json') as userinfo:
     userdict = load(userinfo)
 user = userdict['username']
 password = userdict['password']
-
-insult = ['super ugly', 'an absolute dumbass', 'as useless as a car with 3 wheels',                              #Insults can be disabled
-          'single af', ' a simp', 'Hella Emo', 'so cute', 'a Weirdo', 'Lou', 'an asshole', 'dumb', 'wholesome',  #by changing up some code on
-          'stupid', 'awesome', 'a show-off-person', 'an asshole', 'butterfingers', 'a third wheel to everyone',  #line 84
-          'retarded', 'a tiktok star', 'fat']
-
-loc_no_pic_taken_og = r''  #Whole path of 'No picture taken on this day' image
-loc_whole_path_Imagebin = r''  #Whole Path to ImageBin Dir
-loc_no_pic_taken_Imagebin = r''  #Path of 'No picture taken on this day' inside ImageBin
-loc_error_image_og = r''  #Whole Path of 'Error' image
-loc_error_image_Imagebin = r''  #Path of 'Error' inside ImageBin
+other_user = userdict['other_user']
+# -------- IMPORTANT PATHS --------
+loc_no_pic_taken_og = r''  # Whole path of 'No picture taken on this day' image
+loc_whole_path_image_bin = r''  # Whole Path to ImageBin Dir
+loc_no_pic_taken_image_bin = r''  # Path of 'No picture taken on this day' inside ImageBin
+loc_error_image_og = r''  # Whole Path of 'Error' image
+loc_error_image_image_bin = r''  # Path of 'Error' inside ImageBin
+# -------- HASHTAGS --------
+base_hashtags = "#coding #python #automation #instabot #instapy #google #photos #github #instagram #pycharm #f4f "
+daily_hashtags = {'Monday': '#MondayMemories #MotivationMonday #MondayMotivation #MondayBlues ',
+                  'Tuesday': '#TastingTuesday #TechTuesday #TipTuesday #TopicTuesday #TransformationTuesday ',
+                  'Wednesday': '#HumpDay #WayBackWednesday #WonderfulWednesday #WellnessWednesday #WednesdayWisdom ',
+                  'Thursday': '#ThankfulThursday #ThirstyThursday #ThrowbackThursday #TBT #ThursdayThoughts ',
+                  'Friday': '#FearlessFriday #FlashBackFriday #FridayFun #Friyay #FridayVibes ',
+                  'Saturday': '#SaturdaySwag #SaturdayStyle #SalesSaturday #SelfieSaturday #ShoutoutSaturday ',
+                  'Sunday': '#StartupSunday #SundaySweat #SelfieSunday #SundayFunday #Weekend '}
+no_photo_hashtags = "#nophoto #serious #ugly #me #bored "
+error_photo_hashtags = '#error #mistake #fix #stackoverflow #oops '
 
 
 def download_file(url: str, destination_folder: str, file_name: str):
@@ -39,10 +49,13 @@ def download_file(url: str, destination_folder: str, file_name: str):
             f.write(response.content)
 
 
-log(user, password)  #log in to Instagram
+log(user, password)  # log in to Instagram
+
+# -------- START OF PROGRAM --------
 
 try:
     from init_photo_service import service
+
     response = service.mediaItems().list(pageSize=100).execute()
     list_medias = response.get("mediaItems")
     nextPageToken = response.get('nextPageToken')
@@ -54,9 +67,9 @@ try:
         ).execute()
         list_medias.extend(response.get("mediaItems"))
         nextPageToken = response.get("nextPageToken")
-    df_media_items = DataFrame(list_medias)  #around 200 Items (100 * 2)
+    df_media_items = DataFrame(list_medias)  # around 200 Items (100 * 2)
 
-    feature3 = [d.get('creationTime') for d in df_media_items.mediaMetadata][0][:10]  #last date of last taken photo
+    feature3 = [d.get('creationTime') for d in df_media_items.mediaMetadata][0][:10]  # last date of last taken photo
     meta = (df_media_items[['id', 'mediaMetadata', 'baseUrl']])
     for index, row in meta.iterrows():
         dates = (row['mediaMetadata']['creationTime'][:10])
@@ -68,45 +81,66 @@ try:
             j = j + '\n'
             file_obj.write(j)
         holy_image = choice(Base_URL_list)
-        holy_image = holy_image + "=w1080-h1080-c"  #size of Image so ratio is 1:1
+        holy_image = holy_image + "=w1080-h1080-c"  # size of Image so ratio is 1:1
 
         download_file(holy_image, r'.\ImageBin', "yolo.jpg")
-        cap = f"This is a random photo from @{user}'s Google Photo\nThis photo was shot on: {feature3} GMT\nThis photo had a 1 in {len(Base_URL_list)} Chance "
+        cap = f"""Hi! If this is the first time you are seeing these types of posts, read my bio for more info
+            This is an automatic account which uploads a picture from @{other_user} 's Google Photos!
+
+            This photo was shot on: {feature3} GMT
+            This photo had a 1 in {len(Base_URL_list)} Chance of being uploaded
+            ---- HASHTAGS ----
+            {base_hashtags}{daily_hashtags[today]}"""
         upload_to_instagram(r'.\ImageBin\yolo.jpg', cap)
         if os.path.isfile(r'.\ImageBin\yolo.jpg.REMOVE_ME'):
             os.remove(r'.\ImageBin\yolo.jpg.REMOVE_ME')
 
     else:
         file_obj.write(f'-------{cur}-------\n')
-        copy(loc_no_pic_taken_og, loc_whole_path_Imagebin)
-        sleep(20)
+        copy(loc_no_pic_taken_og, loc_whole_path_image_bin)
+
         file_obj.write('.....NO IMAGE.....\n')
-        upload_to_instagram(loc_no_pic_taken_Imagebin, f'No new pictures were taken on {cur} \n @{user} is {choice(insult)} \n\n\n[User Submitted Entries]')
-        if os.path.isfile(loc_no_pic_taken_Imagebin + '.REMOVE_ME'):
-            os.remove(loc_no_pic_taken_Imagebin + 'REMOVE_ME')
+        upload_to_instagram(loc_no_pic_taken_image_bin, f"""Hi! If this is the first time you are seeing these types of posts, read my bio for more info 
+                                                        No new pictures were taken on {cur}
+                                                        Cheers!
+                                                        ---- HASHTAGS ----
+                                                        {base_hashtags}{daily_hashtags[today]}{no_photo_hashtags}""")
+        if os.path.isfile(loc_no_pic_taken_image_bin + '.REMOVE_ME'):
+            os.remove(loc_no_pic_taken_image_bin + '.REMOVE_ME')
+
+
 except Exception:
     try:
         caps = traceback.format_exc().split("\n")[-3]
         file_obj.write(f'-------{cur}-------\n')
         copy(loc_error_image_og,
-             loc_whole_path_Imagebin)
-        sleep(10)
+             loc_whole_path_image_bin)
+
         file_obj.write(f'..... {traceback.format_exc()} .....\n')
-        upload_to_instagram(loc_error_image_Imagebin,
-                            f'{caps} Occured on {cur} @{user} is a total Coding noob \nHopefully it gets fixed by tomorrow')
-        sleep(10)
-        if os.path.isfile(loc_error_image_Imagebin + ".REMOVE_ME"):
-            os.remove(loc_error_image_Imagebin)
+        upload_to_instagram(loc_error_image_image_bin,
+                            f"""Hi! If this is the first time you are seeing these types of posts, read my bio for more info
+
+                            YAY!!! {caps} Occurred on {cur} @{other_user} really needs to redo his code 
+                            Hopefully it gets fixed by tomorrow
+                            ---- HASHTAGS ----
+                            {base_hashtags}{daily_hashtags[today]}{error_photo_hashtags}""")
+
+        if os.path.isfile(loc_error_image_image_bin + '.REMOVE_ME'):
+            os.remove(loc_error_image_image_bin + '.REMOVE_ME')
     except IndexError:
         copy(loc_error_image_og,
-             loc_whole_path_Imagebin)
+             loc_whole_path_image_bin)
         file_obj.write(f'..... {traceback.format_exc()} .....\n')
-        sleep(10)
-        upload_to_instagram(loc_error_image_Imagebin,
-                            f"Something Happened, I'm not sure what happened cuz this part of the code was never supposed to run YOLO")
-        sleep(10)
-        if os.path.isfile(loc_error_image_Imagebin + '.REMOVE_ME'):
-            os.remove(loc_error_image_Imagebin + '.REMOVE_ME')
+
+        upload_to_instagram(loc_error_image_image_bin,
+                            f"""Hi! If this is the first time you are seeing these types of posts, read my bio for more info
+
+                            An Index error happened... This wasn't supposed to happen @{other_user} you kinda messed up
+                            ---- HASHTAGS ----
+                            {base_hashtags}{daily_hashtags[today]}{error_photo_hashtags}""")
+
+        if os.path.isfile(loc_error_image_image_bin + '.REMOVE_ME'):
+            os.remove(loc_error_image_image_bin + '.REMOVE_ME')
 
 file_obj.close()
 quit()
