1c9f2875cbe98d4259f79d6d33564b68fe4aa739
Aryan Garg <aryan.raj.garg@gmail.com>
2021-02-27 23:19:13 +0530

    Discovered productUrl is a thing, made picture_log which saves the Original URL (not baseUrl) so that you can access them anytime

--- Downloading_random_image_from_Google_Photos.py
+++ Downloading_random_image_from_Google_Photos.py
@@ -12,25 +12,30 @@ import calendar
 
 # -------- ^ Initiations || v META CODE --------
 Base_URL_list = []
-x = datetime.datetime.now()
-cur = x.strftime('%Y-%m-%d')
-my_date = date.today()
-today = calendar.day_name[my_date.weekday()]
-file_obj = open(r'audit_log.txt', 'a+')
+Product_URL_List = []
 
+cur = datetime.datetime.now().strftime('%Y-%m-%d')
+today = calendar.day_name[date.today().weekday()]
+
+audit_log = open(r'audit_log.txt', 'a+')
+picture_log = open(r'picture_log.txt', 'a+')
 with open('./userinfo.json') as userinfo:
-    userdict = load(userinfo)
-user = userdict['username']
-password = userdict['password']
-other_user = userdict['other_user']
+    user_dictionary = load(userinfo)
+user = user_dictionary['username']
+password = user_dictionary['password']
+other_user = user_dictionary['other_user']
+if len(other_user) <= 2:
+    other_user = user
+
 # -------- IMPORTANT PATHS --------
-loc_no_pic_taken_og = r'C:\Users\defaultuser100000\Google Meets Intagram\yolo1.jpg'  # Whole path of 'No picture taken on this day' image
-loc_whole_path_image_bin = r'C:\Users\defaultuser100000\Google Meets Intagram\ImageBin'  # Whole Path to ImageBin Dir
-loc_no_pic_taken_image_bin = r'.\ImageBin\yolo1.jpg'  # Path of 'No picture taken on this day' inside ImageBin
-loc_error_image_og = r'C:\Users\defaultuser100000\Google Meets Intagram\yolo2.jpg'  # Whole Path of 'Error' image
-loc_error_image_image_bin = r'.\ImageBin\yolo2.jpg'  # Path of 'Error' inside ImageBin
+loc_no_pic_taken_og = r''  # Whole path of 'No picture taken on this day' image
+loc_whole_path_image_bin = r''  # Whole Path to ImageBin Dir
+loc_no_pic_taken_image_bin = r''  # Path of 'No picture taken on this day' inside ImageBin
+loc_error_image_og = r''  # Whole Path of 'Error' image
+loc_error_image_image_bin = r''  # Path of 'Error' inside ImageBin
+
 # -------- HASHTAGS --------
-base_hashtags = "#coding #python #automation #instabot #instapy #google #photos #github #instagram #pycharm #f4f #programming #project #pandas #datascience #💻"
+base_hashtags = "#coding #python #automation #instabot #instapy #google #photos #github #instagram #pycharm #f4f #programming #project #pandas #datascience #💻 "
 daily_hashtags = {'Monday': '#MondayMemories #MotivationMonday #MondayMotivation #MondayBlues ',
                   'Tuesday': '#TastingTuesday #TechTuesday #TipTuesday #TopicTuesday #TransformationTuesday ',
                   'Wednesday': '#HumpDay #WayBackWednesday #WonderfulWednesday #WellnessWednesday #WednesdayWisdom ',
@@ -52,7 +57,6 @@ def download_file(url: str, destination_folder: str, file_name: str):
 log(user, password)  # log in to Instagram
 
 # -------- START OF PROGRAM --------
-
 try:
     from init_photo_service import service  # Initating Google Photos Module last so that error management can happen
 
@@ -70,16 +74,22 @@ try:
     df_media_items = DataFrame(list_medias)  # around 200 Items (100 * 2)
 
     feature3 = [d.get('creationTime') for d in df_media_items.mediaMetadata][0][:10]  # last date of last taken photo
-    meta = (df_media_items[['id', 'mediaMetadata', 'baseUrl']])
+    meta = (df_media_items[['id', 'mediaMetadata', 'baseUrl', 'productUrl']])
     for index, row in meta.iterrows():
         dates = (row['mediaMetadata']['creationTime'][:10])
         if dates == feature3:
             Base_URL_list.append(row['baseUrl'])
+            Product_URL_List.append(row['productUrl'])
     if cur == feature3:
-        file_obj.write(
-            f'-------{feature3}-------\nUploaded Picture from Google Photos at {datetime.datetime.time(datetime.datetime.now()).strftime("%H:%M:%S")}\n')
         holy_image = choice(Base_URL_list)
+        new_meta = meta.where(meta['baseUrl'] == holy_image).sort_values(by='productUrl')
+        holy_productUrl = new_meta.iloc[0].productUrl
         holy_image = holy_image + "=w1080-h1080-c"  # size of Image so ratio is 1:1
+        audit_log.write(
+            f'-------{feature3}-------\nUploaded Picture from Google Photos at {datetime.datetime.time(datetime.datetime.now()).strftime("%H:%M:%S")} ({holy_productUrl})\n')
+        picture_log.write(f'-------{feature3}-------\n')
+        for j in Product_URL_List:
+            picture_log.write(f'{j}\n')
 
         download_file(holy_image, r'.\ImageBin', "yolo.jpg")
         cap = f"""Hi! If this is the first time you are seeing these types of posts, read my bio for more info
@@ -94,11 +104,13 @@ try:
             os.remove(r'.\ImageBin\yolo.jpg.REMOVE_ME')
 
     else:
-        file_obj.write(f'-------{cur}-------\n')
+        audit_log.write(f'-------{cur}-------\n')
+        picture_log.write(f'-------{cur}-------\n')
         copy(loc_no_pic_taken_og, loc_whole_path_image_bin)
 
-        file_obj.write(
+        audit_log.write(
             f'Uploaded yolo1.jpg (No new photos were taken today) [{datetime.datetime.time(datetime.datetime.now()).strftime("%H:%M:%S")}]\n')
+        picture_log.write(f'No Pictures were uploaded today')
         upload_to_instagram(loc_no_pic_taken_image_bin, f"""Hi! If this is the first time you are seeing these types of posts, read my bio for more info 
                                                         No new pictures were taken on {cur}
                                                         Cheers!
@@ -111,11 +123,13 @@ try:
 except Exception:
     try:
         caps = traceback.format_exc().split("\n")[-3]
-        file_obj.write(f'-------{cur}-------\n')
+        audit_log.write(f'-------{cur}-------\n')
+        picture_log.write(f'-------{cur}-------\n')
         copy(loc_error_image_og,
              loc_whole_path_image_bin)
 
-        file_obj.write(f' {traceback.format_exc()} \n')
+        audit_log.write(f' {traceback.format_exc()} \n')
+        picture_log.write('An error occured today, check audit_logs.txt for more info')
         upload_to_instagram(loc_error_image_image_bin,
                             f"""Hi! If this is the first time you are seeing these types of posts, read my bio for more info
 
@@ -129,7 +143,7 @@ except Exception:
     except IndexError:
         copy(loc_error_image_og,
              loc_whole_path_image_bin)
-        file_obj.write(f'..... {traceback.format_exc()} .....\n')
+        audit_log.write(f'..... {traceback.format_exc()} .....\n')
 
         upload_to_instagram(loc_error_image_image_bin,
                             f"""Hi! If this is the first time you are seeing these types of posts, read my bio for more info
@@ -140,6 +154,6 @@ except Exception:
 
         if os.path.isfile(loc_error_image_image_bin + '.REMOVE_ME'):
             os.remove(loc_error_image_image_bin + '.REMOVE_ME')
-
-file_obj.close()
+picture_log.close()
+audit_log.close()
 quit()
